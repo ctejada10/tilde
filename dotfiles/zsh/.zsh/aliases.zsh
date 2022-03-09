ad1ed50f37c16939ac5956d0fb3e98929d341152
Carlos E. Tejada <carlos@carlosetejada.com>
2022-03-09 19:51:23 +0100

    Current terminal setup on M1 Pro on '21 MBP

---
+++ dotfiles/zsh/.zsh/aliases.zsh
@@ -0,0 +1,29 @@
+# Colorize output, add file type indicator, and put sizes in human readable format
+alias ls='ls -GFh'
+
+# Same as above, but in long listing format
+alias ll='ls -GFhl'
+
+alias vi=vim
+alias svi='sudo vim'
+alias tree='tree -lsh --du'
+
+#alias ipython='clear; ipython2 --classic --no-banner'
+alias whereami='uname -n'
+
+alias docker-stop-all='docker stop $(docker ps -aq)'
+alias docker-rmi-all='docker rmi $(docker images -q)'
+alias docker-rm-all='docker rm $(docker ps -aq)'
+
+alias update='sudo apt -y update; sudo apt -y upgrade; sudo apt autoremove; sudo apt autoclean'
+alias subs='subliminal download -l'
+
+alias grep='grep --color=auto'
+
+alias mkdir='mkdir -p'
+
+alias plex='docker-compose -f /home/ctejada/plex/etc/docker/docker-compose.yaml'
+
+alias tmux='tmux -u'
+
+alias mount-google-drive='rclone cmount GDrive:/ /tmp/mounts --daemon'
